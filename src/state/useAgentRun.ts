1fe78a20b1a2ae02ed68f3b92f553527fe8d778f
Lalman <lalmanchaudhary2@gmail.com>
2026-04-30 17:13:39 +0530

    feat: introduce useAgentRun hook to manage event-driven state with lifecycle cleanup

---
+++ src/state/useAgentRun.ts
@@ -0,0 +1,27 @@
+import { useEffect, useReducer, useRef } from "react";
+import { reducer, initialState } from "./reducer";
+import type { Event } from "./types";
+import { MockEventEmitter } from "../mock/eventEmitter";
+
+export function useAgentRun(events: Event[]) {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const emitterRef = useRef<MockEventEmitter | null>(null);
+
+  useEffect(() => {
+    const emitter = new MockEventEmitter(events);
+    emitterRef.current = emitter;
+
+    emitter.subscribe((event) => {
+      dispatch(event);
+    });
+
+    emitter.start();
+
+    return () => {
+      emitter.stop();
+    };
+  }, [events]);
+
+  return state;
+}
