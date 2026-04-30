49773b41fda22a7ecf90748494b8f899f64f1256
Lalman <lalmanchaudhary2@gmail.com>
2026-04-30 18:31:00 +0530

    refactor: update TaskList to render grouped parallel tasks

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,12 +1,21 @@
 import type { RunState } from "../state/types";
 import { TaskItem } from "./TaskItem";
+import { groupTasks } from "../utils/groupTasks";
+import { ParallelGroup } from "./ParallelGroup";
 
 export function TaskList({ state }: { state: RunState }) {
-  return (
-    <div className="space-y-3 mt-4">
-      {state.taskOrder.map((taskId) => {
-        const task = state.tasks[taskId];
+  const grouped = groupTasks(state);
 
+  return (
+    <div className="space-y-4 mt-4">
+      {grouped.map((item, index) => {
+        if (Array.isArray(item)) {
+          const tasks = item.map((id) => state.tasks[id]);
+
+          return <ParallelGroup key={index} tasks={tasks} />;
+        }
+
+        const task = state.tasks[item];
         return <TaskItem key={task.id} task={task} />;
       })}
     </div>
